f318fa6c8b1b676268860873d6e181247697da7f
Alexey_Tsibikin <alex999333444@gmail.com>
2025-07-14 20:45:02 +0400

    init

---
+++ src/client/java/net/oxcodsnet/roadarchitect/RoadArchitectClient.java
@@ -0,0 +1,10 @@
+package net.oxcodsnet.roadarchitect;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoadArchitectClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/oxcodsnet/roadarchitect/RoadArchitectDataGenerator.java
@@ -0,0 +1,11 @@
+package net.oxcodsnet.roadarchitect;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RoadArchitectDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
