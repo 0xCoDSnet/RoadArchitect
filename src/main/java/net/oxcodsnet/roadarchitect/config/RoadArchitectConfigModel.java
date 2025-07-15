70727f8ac220ef8874f7f36e7e95cb445c5d78ac
Alexey_Tsibikin <alex999333444@gmail.com>
2025-07-15 21:10:45 +0400

    RoadArchitectConfigModel ---

--- src/main/java/net/oxcodsnet/roadarchitect/config/RoadArchitectConfigModel.java
+++ src/main/java/net/oxcodsnet/roadarchitect/config/RoadArchitectConfigModel.java
@@ -1,7 +1,10 @@
 package net.oxcodsnet.roadarchitect.config;
 
 import io.wispforest.owo.config.annotation.Config;
+import io.wispforest.owo.config.annotation.Modmenu;
+import net.oxcodsnet.roadarchitect.RoadArchitect;
 
+@Modmenu(modId = RoadArchitect.MOD_ID)
 @Config(name = "roadarchitect", wrapperName = "RoadArchitectConfig")
 public class RoadArchitectConfigModel {
     public int playerScanRadius = 100;
