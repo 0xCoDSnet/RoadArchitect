97dd4563491c699a689af32c224df684be54a01f
0xCoDSnet <76873593+0xcodsnet@users.noreply.github.com>
2025-08-14 14:04:09 +0400

    1.1.0

--- src/client/java/net/oxcodsnet/roadarchitect/RoadArchitectClient.java
+++ src/client/java/net/oxcodsnet/roadarchitect/RoadArchitectClient.java
@@ -40,13 +40,8 @@ public class RoadArchitectClient implements ClientModInitializer {
     public void onInitializeClient() {
 
         ScreenEvents.AFTER_INIT.register((client, screen, w, h) -> {
-            // 1) Не трогаем наш же экран
             if (screen instanceof MyLevelLoadingScreen) return;
-
-            // 2) Подменяем только ванильный загрузочный
             if (screen instanceof LevelLoadingScreen vanilla) {
-                // ВАЖНО: не вызываем init() вручную — setScreen сам всё сделает
-                // Дополнительно завернём в client.execute, чтобы выйти из текущего init-колбэка
                 client.execute(() -> client.setScreen(new MyLevelLoadingScreen(vanilla)));
             }
         });
