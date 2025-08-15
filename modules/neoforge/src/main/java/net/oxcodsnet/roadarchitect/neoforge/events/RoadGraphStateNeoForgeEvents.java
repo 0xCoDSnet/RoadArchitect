7537500f8170febdabaaeaae8b33e9aae95fabe3
0xCoDSnet <76873593+0xcodsnet@users.noreply.github.com>
2025-08-16 00:09:58 +0400

    RoadGraphStateNeoForgeEvents

--- modules/neoforge/src/main/java/net/oxcodsnet/roadarchitect/neoforge/events/RoadGraphStateNeoForgeEvents.java
+++ modules/neoforge/src/main/java/net/oxcodsnet/roadarchitect/neoforge/events/RoadGraphStateNeoForgeEvents.java
@@ -1,9 +1,51 @@
 package net.oxcodsnet.roadarchitect.neoforge.events;
 
+import net.minecraft.server.world.ServerWorld;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import net.neoforged.neoforge.event.server.ServerStoppingEvent;
+import net.oxcodsnet.roadarchitect.RoadArchitect;
+import net.oxcodsnet.roadarchitect.storage.RoadGraphState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * NeoForge event handlers for {@link RoadGraphState} lifecycle.
+ */
 public final class RoadGraphStateNeoForgeEvents {
-    private RoadGraphStateNeoForgeEvents() {}
+    private static final Logger LOGGER = LoggerFactory.getLogger(RoadArchitect.MOD_ID + "/RoadGraphStateEvents");
+
+    private RoadGraphStateNeoForgeEvents() {
+    }
 
     public static void register() {
-        // TODO: implement RoadGraphState events for NeoForge
+        NeoForge.EVENT_BUS.register(RoadGraphStateNeoForgeEvents.class);
+    }
+
+    @SubscribeEvent
+    public static void onLevelLoad(LevelEvent.Load event) {
+        if (event.getLevel() instanceof ServerWorld world) {
+            RoadGraphState.get(world, RoadArchitect.CONFIG.maxConnectionDistance());
+            LOGGER.debug("RoadGraphState loaded for world {}", world.getRegistryKey().getValue());
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLevelUnload(LevelEvent.Unload event) {
+        if (event.getLevel() instanceof ServerWorld world) {
+            RoadGraphState state = RoadGraphState.get(world, RoadArchitect.CONFIG.maxConnectionDistance());
+            state.markDirty();
+            LOGGER.debug("Saved RoadGraphState for world {} on unload", world.getRegistryKey().getValue());
+        }
+    }
+
+    @SubscribeEvent
+    public static void onServerStopping(ServerStoppingEvent event) {
+        for (ServerWorld world : event.getServer().getWorlds()) {
+            RoadGraphState state = RoadGraphState.get(world, RoadArchitect.CONFIG.maxConnectionDistance());
+            state.markDirty();
+        }
+        LOGGER.debug("Server stopping, all RoadGraphStates marked dirty");
     }
 }
