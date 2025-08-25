fbea0f38a315ff77d2315036e8e399686154f1a5
0xCoDSnet <76873593+0xcodsnet@users.noreply.github.com>
2025-08-26 01:25:52 +0400

    Приведение кода, к единому стилю ---

--- modules/neoforge/src/main/java/net/oxcodsnet/roadarchitect/neoforge/client/hook/DebugGraphScreenHook.java
+++ modules/neoforge/src/main/java/net/oxcodsnet/roadarchitect/neoforge/client/hook/DebugGraphScreenHook.java
@@ -1,36 +1,28 @@
 package net.oxcodsnet.roadarchitect.neoforge.client.hook;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 import net.minecraft.server.world.ServerWorld;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.InputEvent;
-import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
 import net.oxcodsnet.roadarchitect.RoadArchitect;
 import net.oxcodsnet.roadarchitect.client.gui.RoadGraphDebugScreenVanilla;
 import net.oxcodsnet.roadarchitect.storage.EdgeStorage;
 import net.oxcodsnet.roadarchitect.storage.RoadGraphState;
 import net.oxcodsnet.roadarchitect.storage.components.Node;
-import org.lwjgl.glfw.GLFW;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-
-@EventBusSubscriber(modid = "roadarchitect", value = Dist.CLIENT)
+import static net.oxcodsnet.roadarchitect.neoforge.client.RAKeybinds.OPEN;
+@EventBusSubscriber(
+        modid = RoadArchitect.MOD_ID,
+        bus = EventBusSubscriber.Bus.GAME,
+        value = Dist.CLIENT
+)
 public final class DebugGraphScreenHook {
-    private static KeyBinding OPEN;
-
-    @SubscribeEvent
-    public static void keys(RegisterKeyMappingsEvent e) {
-        OPEN = new KeyBinding("key.roadarchitect.debug", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_H, "category.roadarchitect");
-        e.register(OPEN);
-    }
-
     @SubscribeEvent
     public static void onKey(InputEvent.Key e) {
         if (OPEN != null && OPEN.wasPressed()) {
